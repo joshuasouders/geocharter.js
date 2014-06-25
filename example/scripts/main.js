c3ea66840e8ed191182fbec7a2f4a616b4a47e5f
Josh Souders <rem87062@gmail.com>
2014-06-25 16:14:08 -0400

    got rid of duplicated code, put into function

--- example/scripts/main.js
+++ example/scripts/main.js
@@ -30,17 +30,10 @@ var mydatam1 = {
 var bar1, bar2, bar3, bar4, bar5, bar6, bar7, bar8;
 
 $(document).ready(function(){
-	 bar1 = new Barchart("1", bar, {topLeft: [0,0], bottomRight: [3, 2]}, {graphTitle: "Test 1"});
-	 bar2 = new Barchart("2", mydatam1, {topLeft: [3, 0], bottomRight: [8, 3]}, {graphTitle: "Test 2"});
-	 bar3 = new Barchart("3", bar, {topLeft: [8, 0], bottomRight: [12, 5]}, {graphTitle: "Test 3"});
-	 bar4 = new Barchart("4", bar, {topLeft: [3, 3], bottomRight: [5, 7]}, {graphTitle: "Test 4"});
-	 bar8 = new Barchart("8", mydatam1, {topLeft: [5, 3], bottomRight: [8, 5]}, {graphTitle: "Test 8"});
-	 bar5 = new Barchart("5", mydatam1, {topLeft: [0, 2], bottomRight: [3, 7]}, {graphTitle: "Test 5"});
-	 bar6 = new Barchart("6", bar, {topLeft: [0, 7], bottomRight: [12, 10]}, {graphTitle: "Test 6"});
-	 bar7 = new Barchart("7", bar, {topLeft: [5, 5], bottomRight: [12, 7]}, {graphTitle: "Test 7"});
+	printBarCharts();
 });
 
-function clicked(){
+function printBarCharts(){
 	 bar1 = new Barchart("1", mydatam1, {topLeft: [0,0], bottomRight: [3, 2]}, {graphTitle: "Test 1"});
 	 bar2 = new Barchart("2", bar, {topLeft: [3, 0], bottomRight: [8, 3]}, {graphTitle: "Test 2"});
 	 bar3 = new Barchart("3", mydatam1, {topLeft: [8, 0], bottomRight: [12, 5]}, {graphTitle: "Test 3"});
